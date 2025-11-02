719473b2972decf2a258aac5449966a9b8b780b2
NonsanXX <66070305@kmitl.ac.th>
2025-11-03 02:19:37 +0700

    Refactor app lifecycle management by implementing async context manager for background tasks

--- web/app.py
+++ web/app.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
@@ -5,7 +7,18 @@ from fastapi.responses import FileResponse
 from api import routers as api_routers
 from background import start_background_tasks, stop_background_tasks
 
-app = FastAPI(title="Topologist Web")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    task = start_background_tasks()
+    app.state.interface_task = task
+    try:
+        yield
+    finally:
+        await stop_background_tasks(task)
+
+
+app = FastAPI(title="Topologist Web", lifespan=lifespan)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
@@ -16,19 +29,5 @@ def index():
 @app.get("/info")
 def info_page():
     return FileResponse("static/info.html")
-
-
-@app.on_event("startup")
-async def startup_event():
-    app.state.interface_task = start_background_tasks()
-
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    task = getattr(app.state, "interface_task", None)
-    if task:
-        await stop_background_tasks(task)
-
-
 for router in api_routers:
     app.include_router(router)
